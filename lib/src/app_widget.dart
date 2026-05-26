fea1d9498daa380ade7415ade8667ab34b0f02f3
Pedro Henrique <pedro.henrique.martins404@gmail.com>
2026-05-26 09:05:16 -0300

    feat: refactor AppWidget to use ConsumerWidget and add database client reference

--- lib/src/app_widget.dart
+++ lib/src/app_widget.dart
@@ -1,15 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:nutrinitro/src/core/config/env.dart';
+import 'package:nutrinitro/src/core/database/database_client.dart';
 import 'package:nutrinitro/src/core/themes/app_theme.dart';
 import 'package:nutrinitro/src/ui/splash/splash_page.dart';
 
 final GlobalKey<NavigatorState> appNavigatorKey = GlobalKey<NavigatorState>();
 
-class AppWidget extends StatelessWidget {
+class AppWidget extends ConsumerWidget {
   const AppWidget({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    ref.read(databaseClientProvider.future);
+
     return MaterialApp(
       title: 'NutriNitro',
       navigatorKey: appNavigatorKey,
